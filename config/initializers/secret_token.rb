8493dab537806c88546a0ce87ab6d3272999229b
Chris Oei <chris@eluctari.com>
2011-05-18 07:12:59 -0700

    Initial commit consists of a freshly-created default project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interviews::Application.config.secret_token = '7ea671378ce3bbe9bd28f193571778230db29195d507b4da9e1c2321b65517922140f8020e9e474d3d07158024864967c01ab4d6f3c045b4346ab6ea917e4dfc'
